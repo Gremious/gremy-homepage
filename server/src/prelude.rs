cffa5565c6e72a1c5cc534c048ef00b820e2d3c9
Gremious <gremious@protonmail.com>
2021-10-23 22:47:21 +0100

    Minimal working https setup

--- server/src/prelude.rs
+++ server/src/prelude.rs
@@ -1,49 +1,39 @@
-pub use crate::{
-	// db::{self, DB, heedRwTxnepTExt as _, heedRoTxneTExt as _},
-	// oauth::CredentialsSecret as _,
-};
 pub use actix::prelude::*;
+pub use actix_web::rt::spawn;
 pub use actix_web::web;
 pub use anyhow::Context;
-// pub use async_trait::async_trait;
-// pub use cascade::cascade;
-// pub use itertools::Itertools as _;
+pub use futures::prelude::*;
 pub use once_cell::sync::Lazy;
-// pub use rand::Rng;
 pub use reqwest::header;
 pub use serde::{Deserialize, Serialize};
-pub use shared::{/*enone, serde_jsonValueExt as _, default, */IntoResOpt as _};
-pub use std::{
-	collections::{BTreeMap, HashMap, HashSet},
-	convert::TryFrom,
-};
-// pub use sugars::*;
-// pub use typed_builder::TypedBuilder;
-pub use futures::prelude::*;
-// pub use timed::timed;
-pub use actix_web::rt::spawn;
-// pub use fehler::{throw, throws};
-// pub use smart_default::SmartDefault;
-// pub use chrono::{Datelike as _, TimeZone as _};
+pub use shared::IntoResOpt as _;
+pub use std::collections::{BTreeMap, HashMap, HashSet};
+pub use std::convert::TryFrom;
 
-#[derive(Serialize, Deserialize)]
-#[serde(deny_unknown_fields)]
-pub struct SslConfig {
-	pub key: String,
-	pub cert: String,
-}
+pub static CONFIG: Lazy<Config> = Lazy::new(|| toml::from_str(include_str!("../Config.toml")).expect("failed to parse config"));
 
 #[derive(Serialize, Deserialize)]
 #[serde(deny_unknown_fields)]
 pub struct Config {
-	pub port: String,
-	pub salt: String,
-	pub ssl: SslConfig,
+    pub http_port: u32,
+    pub https_port: u32,
+    pub https: bool,
+    pub salt: String,
+    pub ssl: SslConfig,
 }
 
-pub static CONFIG: Lazy<Config> = Lazy::new(|| toml::from_str(include_str!("../Config.toml")).expect("failed to parse config"));
-pub static CLIENT: Lazy<reqwest::Client> = Lazy::new(reqwest::Client::new);
+#[derive(Serialize, Deserialize)]
+#[serde(deny_unknown_fields)]
+pub struct SslConfig {
+    pub key: String,
+    pub cert: String,
+}
 
+#[allow(dead_code)]
 pub fn spawn_complain<T>(x: impl Future<Output = anyhow::Result<T>> + 'static) {
-	spawn(async move { if let Err(e) = x.await { log::error!("{:?}", e); } });
+    spawn(async move {
+        if let Err(e) = x.await {
+            log::error!("{:?}", e);
+        }
+    });
 }
--- shared/src/lib.rs
+++ shared/src/lib.rs
@@ -1,22 +1,21 @@
-use serde::{Serialize, Deserialize};
 use once_cell::sync::Lazy;
+use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize)]
 pub struct Config {
-	pub project_name: String,
-	pub name: String,
-	pub dev: bool,
-	pub hostname: String,
+    pub project_name: String,
+    pub name: String,
+    pub hostname: String,
 }
 
 pub static CONFIG: Lazy<Config> = Lazy::new(|| toml::from_str(include_str!("../../Config.toml")).expect("failed to parse config"));
 
 pub trait IntoResOpt: Sized {
-	fn into_ok<E>(self) -> Result<Self, E>;
-	fn into_some(self) -> Option<Self>;
+    fn into_ok<E>(self) -> Result<Self, E>;
+    fn into_some(self) -> Option<Self>;
 }
 
 impl<T> IntoResOpt for T {
-	fn into_ok<E>(self) -> Result<Self, E> { Ok(self) }
-	fn into_some(self) -> Option<Self> { Some(self) }
+    fn into_ok<E>(self) -> Result<Self, E> { Ok(self) }
+    fn into_some(self) -> Option<Self> { Some(self) }
 }
