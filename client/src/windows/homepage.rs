1456cec8aa14d26751bf77339710e9dac93454dd
Gremious <gremious@protonmail.com>
2021-10-30 19:42:34 +0100

    Font fn

--- client/src/windows/homepage.rs
+++ client/src/windows/homepage.rs
@@ -4,6 +4,32 @@ pub struct Homepage;
 impl Homepage {
     pub fn new_element() -> cmp::Div {
         cmp::div()
+            .class((
+                css::Display::Flex,
+                css::JustifyContent::Center,
+                css::size!(100%),
+                css::background_color!(css::Color { r: 00, g: 15, b: 20, a: 255 }),
+            ))
+            .child(cmp::div()
+                .text("Hello. Ă U Ũ")
+                .class(css::style!(
+                    .& {
+                        css::padding_top!(2.5%),
+                        css::color!(css::color::WHITE),
+                    }
+
+                    @media All && MaxWidth(css::unit!(500 px)) {
+                        .& {
+                            css::font_size!(12 vw),
+                        }
+                    }
+                ))
+                .class_typed::<text::FontTag>(css::class!(text::space_mono::BODY.clone()))
+            )
+    }
+
+    pub fn old_homepage() -> cmp::Div {
+        cmp::div()
             .class((css::Display::Flex, css::JustifyContent::Center, css::size!(100%), css::background_color!(css::color::PINK)))
             .child(cmp::div()
                 .text("Bonanza Extravaganza")
@@ -21,6 +47,9 @@ impl Homepage {
                         }
                     }
                 ))
+            )
+            .child(cmp::a().attr(web_str::href(), "https://www.cat-bounce.com").class(css::size!(100%))
+                .child(cmp::img().attr(web_str::src(), "../public/img/cat.gif").class(css::size!(100%)))
             )
     }
 }
