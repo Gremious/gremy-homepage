58823340a69cd8a781557bb48395f894ce3f0d53
Gremious Grenald <gremious@protonmail.com>
2022-08-02 20:38:07 +0100

    More cleanup

--- server/src/main.rs
+++ server/src/main.rs
@@ -28,96 +28,91 @@ use prelude::*;
 mod prelude;
 
 async fn reply() -> HttpResponse {
-    static REPLY: Lazy<String> = Lazy::new(|| {
+	static REPLY: Lazy<String> = Lazy::new(|| {
 		let js = &std::fs::read_to_string("public/wasm/client.js").expect("couldn't find the js payload");
-        let js = minifier::js::minify(js);
+		let js = minifier::js::minify(js);
 
-        minify::html::minify(&format!(
-            include_str!("../response.html"),
-            js = js,
+		minify::html::minify(&format!(
+			include_str!("../response.html"),
+			js = js,
 			path = format!("{}://{}/public/wasm/client_bg.wasm", if CONFIG.https { "https" } else { "http" }, shared::CONFIG.hostname)
-        ))
-    });
+		))
+	});
 
-    HttpResponse::Ok().body(&REPLY as &str)
+	HttpResponse::Ok().body(&REPLY as &str)
 }
 
 pub fn init_logger() {
-    env_logger::builder()
-        .filter(None, log::LevelFilter::Info)
-        .filter(
-            Some("server"),
-            #[cfg(debug_assertions)] log::LevelFilter::Trace,
-            #[cfg(not(debug_assertions))] log::LevelFilter::Debug,
-        )
-        .format(|buf, record| {
-            use std::io::Write;
+	env_logger::builder()
+		.filter(None, log::LevelFilter::Info)
+		.filter(
+			Some("server"),
+			#[cfg(debug_assertions)] log::LevelFilter::Trace,
+			#[cfg(not(debug_assertions))] log::LevelFilter::Debug,
+			)
+		.format(|buf, record| {
+			use std::io::Write;
 
-            let mut dimmed = buf.style();
-            dimmed.set_color(env_logger::fmt::Color::Rgb(126, 126, 126));
+			let mut dimmed = buf.style();
+			dimmed.set_color(env_logger::fmt::Color::Rgb(126, 126, 126));
 
-            let mut level_style = buf.style();
-            match record.level() {
-                log::Level::Info => level_style.set_color(env_logger::fmt::Color::Blue),
-                log::Level::Debug => level_style.set_color(env_logger::fmt::Color::Cyan),
-                log::Level::Warn => level_style.set_color(env_logger::fmt::Color::Yellow),
-                log::Level::Error => level_style.set_color(env_logger::fmt::Color::Red),
-                log::Level::Trace => &mut dimmed,
-            };
+			let mut level_style = buf.style();
+			match record.level() {
+				log::Level::Info => level_style.set_color(env_logger::fmt::Color::Blue),
+				log::Level::Debug => level_style.set_color(env_logger::fmt::Color::Cyan),
+				log::Level::Warn => level_style.set_color(env_logger::fmt::Color::Yellow),
+				log::Level::Error => level_style.set_color(env_logger::fmt::Color::Red),
+				log::Level::Trace => &mut dimmed,
+			};
 
-            writeln!(
-                buf,
-                "[{time} {level} {module} {file}:{line}] {args}",
-                time = chrono::Utc::now().format("%Y-%m-%d %H:%M:%S%.3f"),
-                level = level_style.value(record.level()),
-                module = record.module_path().unwrap_or("module?"),
-                file = dimmed.value(record.file().unwrap_or("file?")),
-                line = dimmed.value(record.line().unwrap_or(0)),
-                args = record.args(),
-            )
-        })
-        .init();
+			writeln!(
+				buf,
+				"[{time} {level} {module} {file}:{line}] {args}",
+				time = chrono::Utc::now().format("%Y-%m-%d %H:%M:%S%.3f"),
+				level = level_style.value(record.level()),
+				module = record.module_path().unwrap_or("module?"),
+				file = dimmed.value(record.file().unwrap_or("file?")),
+				line = dimmed.value(record.line().unwrap_or(0)),
+				args = record.args(),
+			)
+		})
+	.init();
 }
 
 #[actix::main]
 async fn main() -> anyhow::Result<()> {
-    init_logger();
+	init_logger();
 
-    HttpServer::new(|| {
+	HttpServer::new(|| {
 		App::new()
 			.wrap(Logger::new("%s in %Ts, %b bytes \"%r\""))
 			.wrap(NormalizePath::new(TrailingSlash::Trim))
-			// .wrap(Compress::new(ContentEncoding::Auto))
-        	.wrap(DefaultHeaders::new()
-				.header(CONTENT_TYPE, "text/html; charset=UTF-8")
-        	    .header(ACCESS_CONTROL_ALLOW_ORIGIN, "*")
-            )
 			.service(web::resource("/favicon.ico").to(async || NamedFile::open("public/img/favicon/sparkling_heart.ico")))
 			.service(web::resource("/sitemap.xml").to(async || NamedFile::open("public/sitemap.xml")))
 			.service(web::resource("/robots.txt").to(async || NamedFile::open("public/robots.txt")))
 			.service(actix_files::Files::new("/public", "public").show_files_listing())
 			.default_service(web::route().to(reply))
 	})
-		// .bind(format!("[::]:{}", CONFIG.http_port))?
-		.bind_rustls(format!("[::]:{}", CONFIG.https_port), {
-			let cert_file = &mut BufReader::new(File::open(&CONFIG.ssl.cert)?);
-			let key_file = &mut BufReader::new(File::open(&CONFIG.ssl.key)?);
+	// .bind(format!("[::]:{}", CONFIG.http_port))?
+	.bind_rustls(format!("[::]:{}", CONFIG.https_port), {
+		let cert_file = &mut BufReader::new(File::open(&CONFIG.ssl.cert)?);
+		let key_file = &mut BufReader::new(File::open(&CONFIG.ssl.key)?);
 
-			let cert_chain = certs(cert_file).ok().context("no certs")?.into_iter()
-				.map(rustls::Certificate)
-				.collect::<Vec<_>>();
+		let cert_chain = certs(cert_file).ok().context("no certs")?.into_iter()
+			.map(rustls::Certificate)
+			.collect::<Vec<_>>();
 
-			let mut keys = pkcs8_private_keys(key_file).ok().context("no private keys")?.into_iter()
-				.map(rustls::PrivateKey)
-				.collect::<Vec<_>>();
+		let mut keys = pkcs8_private_keys(key_file).ok().context("no private keys")?.into_iter()
+			.map(rustls::PrivateKey)
+			.collect::<Vec<_>>();
 
-			rustls::ServerConfig::builder()
-				.with_cipher_suites(rustls::DEFAULT_CIPHER_SUITES)
-				.with_safe_default_kx_groups()
-				.with_protocol_versions(rustls::DEFAULT_VERSIONS)?
-				.with_no_client_auth()
-				.with_single_cert(cert_chain, keys.remove(0))?
-		})?
-		.run().await?
+		rustls::ServerConfig::builder()
+			.with_cipher_suites(rustls::DEFAULT_CIPHER_SUITES)
+			.with_safe_default_kx_groups()
+			.with_protocol_versions(rustls::DEFAULT_VERSIONS)?
+			.with_no_client_auth()
+			.with_single_cert(cert_chain, keys.remove(0))?
+	})?
+	.run().await?
 		.into_ok()
 }
