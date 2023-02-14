7e9d93e8a47f97c7d026e601ccd492a2fe04a80c
Gremious Grenald <gremious@gremy.co.uk>
2023-02-14 00:42:16 +0000

    Move things in order of use

--- client/src/pages/homepage.rs
+++ client/src/pages/homepage.rs
@@ -4,7 +4,7 @@ use super::*;
 
 // TODO: Easter eggs
 // - Hide something behind lain image.
-// - Type "hi" - type out smiley face to hello text e.g. "Hello :)_"
+// - Type "hi" - type out smiley face to hello text e.g. "Hello :)_"t
 // - Type "hello" - secret item fly-in?
 //
 
@@ -15,6 +15,108 @@ pub fn new() -> e::Div {
 		.child(lain_image())
 		.child(hello_header())
 		.child(homepage_nav())
+}
+
+fn container() -> e::Div {
+	e::div()
+		.class(css::style!(
+			.& {
+				css::Position::Relative,
+				css::Display::Flex,
+				css::FlexDirection::Column,
+				css::JustifyContent::FlexStart,
+				css::BoxSizing::BorderBox,
+				css::overflow!(hidden),
+				css::padding!(2.5%),
+				css::background_color!(colors::bg_black),
+			}
+
+			@media All && MaxWidth(css::unit!(500 px)) {
+				.& { "background-size: cover;" }
+			}
+		))
+}
+
+fn lain_image() -> e::Div {
+	e::div()
+		.class((
+			css::Position::Absolute,
+			css::top!(50% - 300 px),
+			css::left!(50% - 336 px),
+			css::width!(672 px),
+			css::height!(600 px),
+			css::BackgroundImage::Some(vec![css::Image::Url("../public/img/lain/small.webp".to_string())]),
+			css::BackgroundRepeat::NoRepeat,
+			css::background_color!(colors::bg_black),
+		))
+		.tap(|&element| element.add_on_mouse_down(move |_| {
+			element.set_class_tagged("Cursor", css::class!(css::Cursor::Grabbing));
+			*element.get_cmp_mut::<Clicked>() = Clicked(true);
+		}))
+		.tap(|&element| element.add_on_mouse_up(move |_| {
+			element.set_class_tagged("Cursor", css::class!(css::Cursor::Auto));
+			element.get_cmp_mut::<Clicked>().0 = false;
+		}))
+		.tap(|&element| element.add_on_mouse_leave(move |_| {
+			element.set_class_tagged("Cursor", css::class!(css::Cursor::Auto));
+			element.get_cmp_mut::<Clicked>().0 = false;
+		}))
+		.tap(|&element| element.add_on_mouse_move(move |mouse_event| {
+			if element.get_cmp::<Clicked>().0 {
+				let (move_y, move_x) = (mouse_event.movement_y(), mouse_event.movement_x());
+				let left = element.left() + f64::from(move_x);
+				let top = element.top() + f64::from(move_y);
+
+				element.set_style((css::top!(top), css::left!(left)));
+			}
+		}))
+		.component(Clicked(false))
+}
+
+
+fn hello_header() -> e::Div {
+	e::div()
+		.class(css::style!(
+			.& {
+				css::color!(css::color::WHITE),
+				"text-shadow: 2px 2px 1px black;",
+			}
+
+			raw("@keyframes writing") {
+				"from{ opacity:0 } to { opacity: 1 }"
+			}
+
+			raw("@keyframes caret") {
+				"0% { opacity:0 } 50% { opacity: 1 }"
+			}
+		))
+		.child(e::div()
+			.font(&text::space_mono::BIG)
+			.class(css::Display::Flex)
+			.child(fade_in_typewriter_animated_text("Hello."))
+			.child(blinking_caret())
+		)
+}
+
+#[allow(clippy::cast_precision_loss)]
+pub fn fade_in_typewriter_animated_text(text: &str) -> e::Div {
+	e::div()
+		.children(text.chars().enumerate().map(|(i, c)| -> e::Span {
+			e::span()
+				.text(c.to_string())
+				.class((
+					css::opacity!(0),
+					format!("animation: writing 0.1s linear forwards {}s;", i as f32 * 0.1)
+				))
+		}))
+}
+
+pub fn blinking_caret() -> e::Div {
+	e::div()
+		.child(e::span()
+			.text("_")
+			.class("animation: caret 1.33s steps(1, end) infinite;")
+		)
 }
 
 fn homepage_nav() -> e::Ul {
@@ -87,106 +189,5 @@ fn button(text: &str) -> e::Li {
 				"text-underline-offset: 0.25em;",
 			))
 		)
-}
-
-fn container() -> e::Div {
-	e::div()
-		.class(css::style!(
-			.& {
-				css::Position::Relative,
-				css::Display::Flex,
-				css::FlexDirection::Column,
-				css::JustifyContent::FlexStart,
-				css::BoxSizing::BorderBox,
-				css::overflow!(hidden),
-				css::padding!(2.5%),
-				css::background_color!(colors::bg_black),
-			}
-
-			@media All && MaxWidth(css::unit!(500 px)) {
-				.& { "background-size: cover;" }
-			}
-		))
-}
-
-fn hello_header() -> e::Div {
-	e::div()
-		.class(css::style!(
-			.& {
-				css::color!(css::color::WHITE),
-				"text-shadow: 2px 2px 1px black;",
-			}
-
-			raw("@keyframes writing") {
-				"from{ opacity:0 } to { opacity: 1 }"
-			}
-
-			raw("@keyframes caret") {
-				"0% { opacity:0 } 50% { opacity: 1 }"
-			}
-		))
-		.child(e::div()
-			.font(&text::space_mono::BIG)
-			.class(css::Display::Flex)
-			.child(fade_in_typewriter_animated_text("Hello."))
-			.child(blinking_caret())
-		)
-}
-
-#[allow(clippy::cast_precision_loss)]
-pub fn fade_in_typewriter_animated_text(text: &str) -> e::Div {
-	e::div()
-		.children(text.chars().enumerate().map(|(i, c)| -> e::Span {
-			e::span()
-				.text(c.to_string())
-				.class((
-					css::opacity!(0),
-					format!("animation: writing 0.1s linear forwards {}s;", i as f32 * 0.1)
-				))
-		}))
-}
-
-pub fn blinking_caret() -> e::Div {
-	e::div()
-		.child(e::span()
-			.text("_")
-			.class("animation: caret 1.33s steps(1, end) infinite;")
-		)
-}
-
-fn lain_image() -> e::Div {
-	e::div()
-		.class((
-			css::Position::Absolute,
-			css::top!(50% - 300 px),
-			css::left!(50% - 336 px),
-			css::width!(672 px),
-			css::height!(600 px),
-			css::BackgroundImage::Some(vec![css::Image::Url("../public/img/lain/small.webp".to_string())]),
-			css::BackgroundRepeat::NoRepeat,
-			css::background_color!(colors::bg_black),
-		))
-		.tap(|&element| element.add_on_mouse_down(move |_| {
-			element.set_class_tagged("Cursor", css::class!(css::Cursor::Grabbing));
-			*element.get_cmp_mut::<Clicked>() = Clicked(true);
-		}))
-		.tap(|&element| element.add_on_mouse_up(move |_| {
-			element.set_class_tagged("Cursor", css::class!(css::Cursor::Auto));
-			element.get_cmp_mut::<Clicked>().0 = false;
-		}))
-		.tap(|&element| element.add_on_mouse_leave(move |_| {
-			element.set_class_tagged("Cursor", css::class!(css::Cursor::Auto));
-			element.get_cmp_mut::<Clicked>().0 = false;
-		}))
-		.tap(|&element| element.add_on_mouse_move(move |mouse_event| {
-			if element.get_cmp::<Clicked>().0 {
-				let (move_y, move_x) = (mouse_event.movement_y(), mouse_event.movement_x());
-				let left = element.left() + f64::from(move_x);
-				let top = element.top() + f64::from(move_y);
-
-				element.set_style((css::top!(top), css::left!(left)));
-			}
-		}))
-		.component(Clicked(false))
 }
 
