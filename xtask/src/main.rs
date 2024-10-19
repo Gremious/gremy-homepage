9a56a72047f022d3387c0da3dbf322c621dacd34
Gremious Grenald <gremious@gremy.co.uk>
2024-10-19 13:12:29 +0100

    xtask for client building

--- xtask/src/main.rs
+++ xtask/src/main.rs
@@ -1,3 +1,81 @@
-fn main() {
-    println!("Hello, world!");
+use std::env;
+use clap::{Parser, Subcommand};
+use execute::{command, Execute};
+use shared::shared_prelude::*;
+
+
+#[derive(Parser)]
+#[command(bin_name = "cargo xtask")]
+struct Cli {
+	#[command(subcommand)]
+	command: Command,
 }
+
+#[derive(Subcommand)]
+enum Command {
+	Build {
+		#[arg(short, long)] release: bool,
+	},
+	BuildClient {
+		#[arg(short, long)] release: bool,
+	},
+	WatchClient,
+	WatchServer,
+	Deploy,
+}
+
+fn main() {
+	let cli = Cli::parse();
+	handle_command(cli.command).unwrap();
+}
+
+fn command_output(cmd: impl AsRef<str>) -> anyhow::Result<String> {
+	Ok(command(cmd).output()?.stdout.pipe(String::from_utf8)?)
+}
+
+fn handle_command(cmd: Command) -> anyhow::Result<()> {
+	match cmd {
+		// wasm-pack, now in ~30 lines of code
+		//
+		// DWARF-based debugging with var value in scope, stepping etc works with chrome plugin:
+		// https://chromewebstore.google.com/detail/cc++-devtools-support-dwa/pdcpmagijalfljmkmjngeonclgbbannb
+		Command::BuildClient { release } => {
+			if command("wasm-bindgen -V").status().is_err() {
+				command("cargo install wasm-bindgen-cli -f").status()?;
+			}
+			if command("wasm-opt --version").status().is_err() {
+				command("cargo install wasm-opt -f").status()?;
+			}
+			if !command_output("rustup target list --installed")?.lines().any(|x| x == "wasm32-unknown-unknown") {
+				command("rustup target add wasm32-unknown-unknown").status()?;
+			}
+
+			let build_client = format!("cargo build -p client --target wasm32-unknown-unknown --target-dir target/wasm-target {}",
+				if release { "--profile wasm-release" } else { "" }
+			);
+			command(build_client).status()?;
+
+			let wasm_bindgen_params = if release {
+				"--remove-name-section --remove-producers-section target/wasm-target/wasm32-unknown-unknown/wasm-release/client.wasm"
+			// } else if shared::CONFIG.debug_wasm {
+				// "--debug --keep-debug target/wasm-target/wasm32-unknown-unknown/debug/client.wasm"
+			} else {
+				"--debug target/wasm-target/wasm32-unknown-unknown/debug/client.wasm"
+			};
+
+		let wasm_bindgen = format!("wasm-bindgen --out-dir public/wasm --out-name code --target web --no-typescript --omit-imports --omit-default-module-path --reference-types {wasm_bindgen_params}");
+			command(wasm_bindgen).status()?;
+
+			if release {
+				command("wasm-opt public/wasm/code_bg.wasm --enable-reference-types -o public/wasm/code_bg.wasm -Os").status()?;
+			}
+		},
+		Command::Build { release } => todo!(),
+		Command::WatchClient => todo!(),
+		Command::WatchServer => todo!(),
+		Command::Deploy => todo!(),
+	};
+
+	Ok(())
+}
+
