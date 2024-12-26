39970e3459ce099ded652b45ca808c7fe3918fac
Gremious Grenald <gremious@gremy.co.uk>
2024-12-26 21:56:04 +0000

    Clean up a bit

--- client/src/pages/real.rs
+++ client/src/pages/real.rs
@@ -1,7 +1,7 @@
 use crate::prelude::*;
 mod one;
+mod main;
 use crate::pages::Clicked;
-// use crate::widgets as w;
 
 pub fn new() -> e::Div {
 	let parent = e::div();
@@ -11,60 +11,41 @@ pub fn new() -> e::Div {
 		one::new().text("two"),
 		one::new().text("three"),
 		one::new().text("four"),
-		one::new().text("five"),
+		main::new(),
 		one::new().text("six"),
 		one::new().text("seven"),
 		one::new().text("eight"),
 		one::new().text("nine"),
 	];
+
 	let center = grid_items[4];
 
 	parent
-		.tap(|&ele| {
-			ele.add_on_dom_attach(move || {
-				let viewport_height = document().document_element().unwrap().client_height() as f64;
-				let viewport_width = document().document_element().unwrap().client_width() as f64;
-
-				if viewport_width < viewport_height {
-					for child in &grid_items {
-						child.set_style((
-							css::width::vw(100),
-						));
-					}
-				} else {
-					for child in &grid_items {
-						child.set_style(css::height::vh(100));
-					}
-				}
-
-
-				let body_left = document().body().unwrap().get_bounding_client_rect().left();
-				let body_top = document().body().unwrap().get_bounding_client_rect().top();
-
-				let ele_top = center.top();
-				let ele_left = center.left();
-
-				let viewport_height = document().document_element().unwrap().client_height();
-				let viewport_width = document().document_element().unwrap().client_width();
-
-				let extra_height = viewport_height as f64 - center.height();
-				let extra_width = viewport_width as f64 - center.width();
-
-				let top = (ele_top - body_top) - (extra_height / 2.0);
-				let left = ele_left - body_left - (extra_width / 2.0);
-
-				log::debug!("top: {}, left: {}", top,  left);
-				window().scroll_to_with_x_and_y(left, top);
-			})
-		})
 		.class((
 			css::display::grid,
+			css::user_select::none,
 			"grid-template-columns: repeat(3, 1fr);",
 			"grid-template-rows: repeat(3, 1fr);",
 			css::color::rgba(css::colors::WHITE)
 		))
+		.on_next_flow(move || {
+			let viewport_height = document().document_element().unwrap().client_height() as f64;
+			let viewport_width = document().document_element().unwrap().client_width() as f64;
+
+			if viewport_width < viewport_height {
+				for child in &grid_items {
+					child.set_style(css::width::vw(100));
+				}
+			} else {
+				for child in &grid_items {
+					child.set_style(css::height::vh(100));
+				}
+			}
+
+			scroll_to_ele(center);
+		})
 		.component({
-			let closure = move || {
+			let closure = Closure::wrap(Box::new(move || {
 				let child_height = grid_items[0].height();
 				let child_width = grid_items[0].width();
 
@@ -80,11 +61,12 @@ pub fn new() -> e::Div {
 						child.set_style(css::height::vh(100));
 					}
 				}
-			};
 
-			let f = Closure::wrap(Box::new(closure) as Box<dyn FnMut()>);
-			window().set_onresize(Some(f.as_ref().unchecked_ref()));
-			f
+			}) as Box<dyn FnMut()>);
+
+			window().set_onresize(Some(closure.as_ref().unchecked_ref()));
+
+			closure
 		})
 		.children(grid_items)
 		.tap(|&element| element.add_on_mouse_down(move |_| {
@@ -109,3 +91,24 @@ pub fn new() -> e::Div {
 		}))
 		.component(Clicked(false))
 }
+
+fn scroll_to_ele(ele: e::Div) {
+	let body_left = document().body().unwrap().get_bounding_client_rect().left();
+	let body_top = document().body().unwrap().get_bounding_client_rect().top();
+
+	let ele_top = ele.top();
+	let ele_left = ele.left();
+
+	let viewport_height = document().document_element().unwrap().client_height();
+	let viewport_width = document().document_element().unwrap().client_width();
+
+	let extra_height = viewport_height as f64 - ele.height();
+	let extra_width = viewport_width as f64 - ele.width();
+
+	let top = (ele_top - body_top) - (extra_height / 2.0);
+	let left = ele_left - body_left - (extra_width / 2.0);
+
+	log::debug!("top: {}, left: {}", top,  left);
+	window().scroll_to_with_x_and_y(left, top);
+}
+
